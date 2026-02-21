bde947beeb0f6d55fccc90a72b6a1b0138eeaffa
sdrarunvarshan <sdrarunvarshan@gmail.com>
2026-02-21 13:53:08 +0530

    Updated Application

--- app/frontend/lib/utils/razorpay_helper.dart
+++
@@ -1,2 +0,0 @@
-export 'razorpay_helper_stub.dart' if (dart.library.html) 'razorpay_helper_web.dart';
-
--- app/frontend/lib/utils/razorpay_helper_stub.dart
+++
@@ -1,8 +0,0 @@
-void openRazorpay({
-  required Map<String, dynamic> options,
-  required Function(dynamic) successCallback,
-  required Function() dismissCallback,
-}) {
-  // This should never be called on mobile (guarded by kIsWeb), but throw if it is
-  throw UnsupportedError('Razorpay web logic is not supported on this platform');
-}
